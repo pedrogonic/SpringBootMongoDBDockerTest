8f5166e6c4906d3e36d48c55b94e90b4e463e851
Pedro Coelho <pedrogonic@gmail.com>
2020-01-09 20:29:26 -0300

    Building with Dockerfile
    Run maven package and then:
    docker build -t <image_tag> . && docker run -p 8080:8080 --name pedrogonic/dockerspringdemowithdockerfile <image_tag>

---
+++ src/main/java/com/pedrogonic/dockerspringdemo/DockerspringdemoApplication.java
@@ -0,0 +1,24 @@
+package com.pedrogonic.dockerspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerspringdemoApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Docker updated!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerspringdemoApplication.class, args);
+
+        System.out.println("hello!");
+
+    }
+
+}
