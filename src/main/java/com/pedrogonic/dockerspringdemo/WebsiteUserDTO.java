78c9768869ade1a719aa35a0d911a16e1257db36
Pedro Coelho <pedrogonic@gmail.com>
2020-01-19 16:55:43 -0300

    Fixing configs

--- src/main/java/com/pedrogonic/dockerspringdemo/WebsiteUserDTO.java
+++ src/main/java/com/pedrogonic/dockerspringdemo/WebsiteUserDTO.java
@@ -1,12 +1,16 @@
 package com.pedrogonic.dockerspringdemo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class WebsiteUserDTO {
 
